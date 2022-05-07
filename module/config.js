0c9e21648f9c064a8f74cfabf1497cc24a7cbfbd
BarabulkaForever <mark.g.ya@yandex.ru>
2022-05-07 18:26:53 +0300

    Add renge weapons

--- module/config.js
+++ module/config.js
@@ -2,9 +2,20 @@ export const wanderers = {};
 
 wanderers.weaponTypes = {
     none: "wanderers.weaponTypes.none",
-    swords : "wanderers.weaponTypes.swords",
     blades : "wanderers.weaponTypes.blades",
-    polearm : "wanderers.weaponTypes.polearm"
+    polearm : "wanderers.weaponTypes.polearm",
+    special : "wanderers.weaponTypes.special",
+    hidden : "wanderers.weaponTypes.hidden",
+    improvised : "wanderers.weaponTypes.improvised"
+}
+
+wanderers.rangeWeaponTypes = {
+    none: "wanderers.rangeWeaponTypes.none",
+    blackpowder : "wanderers.rangeWeaponTypes.blackpowder",
+    bow : "wanderers.rangeWeaponTypes.bow",
+    crossbow : "wanderers.rangeWeaponTypes.crossbow",
+    throwing : "wanderers.rangeWeaponTypes.throwing",
+    explosives : "wanderers.rangeWeaponTypes.explosives"
 }
 
 wanderers.attackAttributes = {
